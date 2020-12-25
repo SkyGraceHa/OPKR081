5da3be0a02252d687ed94661f4ac40ff564fa115
Chanho Jung <jungch@naver.com>
2020-12-25 12:44:25 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -160,10 +160,10 @@ class PathPlanner():
     x = max(sm['liveParameters'].stiffnessFactor, 0.1)
     
     if self.live_sr:
-      self.live_steerRatio_range = [CP.steerRatio, sm['liveParameters'].steerRatio] #  학습 SR값 범위 설정
-      self.live_steerRatio = interp(abs(anglesteer_current), self.angle_range, self.live_steerRatio_range)
-      # sr = max(sm['liveParameters'].steerRatio, 0.1) #Live SR
-      sr = self.live_steerRatio
+      #self.live_steerRatio_range = [CP.steerRatio, sm['liveParameters'].steerRatio] #  학습 SR값 범위 설정
+      #self.live_steerRatio = interp(abs(anglesteer_current), self.angle_range, self.live_steerRatio_range)
+      sr = max(sm['liveParameters'].steerRatio, 0.1) #Live SR
+      #sr = self.live_steerRatio
     else:
       sr = max(self.new_steerRatio, 0.1) #가변 SR
     VM.update_params(x, sr)
