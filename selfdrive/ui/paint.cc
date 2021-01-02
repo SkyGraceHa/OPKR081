0d264dbb465b72422b3a21117b531715e423ddee
jungch <jungch@naver.com>
2021-01-02 13:57:42 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -174,7 +174,7 @@ static void ui_draw_line(UIState *s, const vertex_data *v, const int cnt, NVGcol
 
 static void update_track_data(UIState *s, const cereal::ModelDataV2::XYZTData::Reader &line, track_vertices_data *pvd) {
   const UIScene *scene = &s->scene;
-  const float off = 1.0; // 0.5 => 1.0 가이드선 굵기를 실제 차량 사이즈로 키우기
+  const float off = 0.3;
   int max_idx = 0;
   float lead_d;
   if (s->sm->updated("radarState")) {
